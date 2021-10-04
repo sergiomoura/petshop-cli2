57a5552080dca2547546e0f746a0f0a8ae9a501c
Sérgio Moura <smouracalmon@gmail.com>
2021-10-04 16:52:00 -0300

    Primeiro commit

---
+++ funcoes.js
@@ -0,0 +1,55 @@
+const cachorros = require('./database/cachorros.json');
+const fs = require('fs');
+
+module.exports = {
+    listar: function(){
+        console.table(cachorros);
+    },
+    descrever: function(pos){
+
+        if(pos >= cachorros.length || pos < 0){
+            console.error("Cachorro inexistente!");
+            return;
+        }
+
+        let c = cachorros[pos];
+        console.log(`Nome: ${c.nome}`);
+        console.log(`Data De Nascimento: ${c.dataDeNascimento}`);
+        console.log (`Peso: ${c.peso}`);
+
+        if(c.castrado){
+            console.log("Castrado: sim")
+        }else{
+            console.log("Castrado: não")
+        }
+
+        console.log(`Sexo: ${c.sexo}`);
+        
+        console.log("Vacinas:")
+        console.table(c.vacinas);
+        
+        console.log("Serviços:")
+        console.table(c.servicos);
+
+    },
+    adicionar: function($nome, $sexo, $castrado, $dataDeNascimento, $peso){
+        // Criar objeto cachorro
+        let dog = {
+            nome: $nome,
+            sexo: $sexo,
+            castrado: $castrado,
+            dataDeNascimento: $dataDeNascimento,
+            peso: $peso,
+            vacinas: [],
+            servicos: []
+        }
+        
+        // Adicionar o cachorro criado a array de cachorros
+        cachorros.push(dog);
+
+        // Gravar array de cachorros no arquivo cachorros.json
+        fs.writeFileSync('./database/cachorros.json', JSON.stringify(cachorros));
+    }
+
+
+}
